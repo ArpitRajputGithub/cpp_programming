2bcbc03ec15d1e0cb0035eba52086a1d8b39693c
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-07-19 10:26:08 +0530

    control statements

---
+++ Week 2/Code/ifelse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // check even odd
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Even";
+    }
+    else
+    {
+        cout << "Odd";
+    }
+}
