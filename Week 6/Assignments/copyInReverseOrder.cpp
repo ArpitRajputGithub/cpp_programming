99c33735bd0134231579e91a826c7b58a358793d
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-08-05 10:32:05 +0530

    Arrays

---
+++ Week 6/Assignments/copyInReverseOrder.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int ele;
+        cin >> ele;
+        v.push_back(ele);
+    }
+
+    vector<int> vRev(v.size());
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        // vRev[ v.size() - 1] = v[i];
+        vRev[i] = v[v.size() - 1 - i];
+    }
+
+    for (int i = 0; i < vRev.size(); i++)
+    {
+        cout << vRev[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
