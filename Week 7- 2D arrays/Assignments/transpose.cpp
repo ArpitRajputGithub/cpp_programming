424b8050e9ed9825e9e70ff9f00b9350881a5f0b
ArpitRajputGithub <work.arpitrajput@gmail.com>
2024-08-12 22:45:08 +0530

    2D arrays

---
+++ Week 7- 2D arrays/Assignments/transpose.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cout << "Enter the number of rows : ";
+    cin >> m;
+    int n;
+    cout << "Enter the number of columns : ";
+    cin >> n;
+
+    int a[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << endl;
+
+    // Transpose
+    int t[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            t[i][j] = a[j][i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << t[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
